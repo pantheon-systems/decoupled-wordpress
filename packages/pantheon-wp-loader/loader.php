87d1e22393379af153878b0c2a39e02cfefe8119
Brian Perry <brian.perry@pantheon.io>
2022-02-10 16:21:41 -0600

    DB-1478: autoload pantheon-decoupled-gatsby

--- packages/pantheon-wp-loader/loader.php
+++ packages/pantheon-wp-loader/loader.php
@@ -18,7 +18,8 @@ $mu_plugins = [
 	'lh-hsts/lh-hsts.php',
 	'pantheon-advanced-page-cache/pantheon-advanced-page-cache.php',
 	'pantheon-wp-muplugin/pantheon.php',
-	'wp-native-php-sessions/pantheon-sessions.php'
+	'wp-native-php-sessions/pantheon-sessions.php',
+  'pantheon-decoupled-gatsby/pantheon-decoupled-gatsby.php'
 ];
 
 foreach ( $mu_plugins as $file ) {
